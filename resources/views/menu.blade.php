cae068e798e36b6541850e396f2c0404dd508e2d
xvhxid <mvahidsm7@gmail.com>
2023-05-12 11:13:48 +0700

    commit

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,116 +1,101 @@
 @extends('layouts.main')
 @section('content')
-		<!-- breadcrumb-section -->
-		<div class="breadcrumb-section breadcrumb-bg">
-			<div class="container">
-				<div class="row">
-					<div class="col-lg-8 offset-lg-2 text-center">
-						<div class="breadcrumb-text">
-							<p>FOOD n SIT</p>
-							<h1>Menu</h1>
-						</div>
+	<!-- breadcrumb-section -->
+	<div class="breadcrumb-section breadcrumb-bg">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-8 offset-lg-2 text-center">
+					<div class="breadcrumb-text">
+						<p>Halaman ini adalah</p>
+						<h1>Menu</h1>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- end breadcrumb section -->
-	
-		<!-- products -->
-		<div class="product-section mt-150 mb-150">
-			<div class="container">
-	
-				<div class="row">
-					<div class="col-md-12">
-						<div class="product-filters">
-							<ul>
-								<li class="active" data-filter="*">All</li>
-								<li data-filter=".strawberry">Strawberry</li>
-								<li data-filter=".berry">Berry</li>
-								<li data-filter=".lemon">Lemon</li>
-								<li data-filter=".apel">apel</li>
-							</ul>
+	</div>
+	<!-- end breadcrumb section -->
+
+	<!-- products -->
+	<div class="product-section mt-150 mb-150">
+		<div class="container">
+
+			<div class="row">
+                <div class="col-md-12">
+                    <div class="product-filters">
+                        <ul>
+                            <li class="active" data-filter="*">All</li>
+                            <li data-filter=".strawberry">main chors</li>
+                            <li data-filter=".berry">dessert</li>
+                            <li data-filter=".lemon">drink</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+			<div class="row product-lists">
+				<div class="col-lg-4 col-md-6 text-center strawberry">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/makanan1.png" alt=""></a>
 						</div>
+						<h3>Lobster Baked</h3>
+						<p class="product-price"><span>Per porsi</span> RP.50.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
 					</div>
 				</div>
-	
-				<div class="row product-lists">
-					<div class="col-lg-4 col-md-6 text-center strawberry">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-1.jpg" alt=""></a>
-							</div>
-							<h3>Strawberry</h3>
-							<p class="product-price"><span>Per Kg</span> 85$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+				<div class="col-lg-4 col-md-6 text-center berry">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/makanan2.jpg" alt=""></a>
 						</div>
-					</div>
-					<div class="col-lg-4 col-md-6 text-center berry">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-2.jpg" alt=""></a>
-							</div>
-							<h3>Berry</h3>
-							<p class="product-price"><span>Per Kg</span> 70$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-6 text-center lemon">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-3.jpg" alt=""></a>
-							</div>
-							<h3>Lemon</h3>
-							<p class="product-price"><span>Per Kg</span> 35$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-6 text-center">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-4.jpg" alt=""></a>
-							</div>
-							<h3>Avocado</h3>
-							<p class="product-price"><span>Per Kg</span> 50$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-6 text-center apel">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-5.jpg" alt=""></a>
-							</div>
-							<h3>Green Apple</h3>
-							<p class="product-price"><span>Per Kg</span> 45$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-						</div>
-					</div>
-					<div class="col-lg-4 col-md-6 text-center strawberry">
-						<div class="single-product-item">
-							<div class="product-image">
-								<a href="single-product.html"><img src="assets/img/products/product-img-6.jpg" alt=""></a>
-							</div>
-							<h3>Strawberry</h3>
-							<p class="product-price"><span>Per Kg</span> 80$ </p>
-							<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
-						</div>
+						<h3>Gelato</h3>
+						<p class="product-price"><span>Per cup</span> RP.35.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
 					</div>
 				</div>
-	
-				<div class="row">
-					<div class="col-lg-12 text-center">
-						<div class="pagination-wrap">
-							<ul>
-								<li><a href="#">Prev</a></li>
-								<li><a href="#">1</a></li>
-								<li><a class="active" href="#">2</a></li>
-								<li><a href="#">3</a></li>
-								<li><a href="#">Next</a></li>
-							</ul>
+				<div class="col-lg-4 col-md-6 text-center lemon">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/minuman1.jpg" alt=""></a>
 						</div>
+						<h3>ice cinamoroll</h3>
+						<p class="product-price"><span>Per cups</span> RP.30.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 text-center">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/minuman2.jpg" alt=""></a>
+						</div>
+						<h3>ice coffe latte</h3>
+						<p class="product-price"><span>Per cups</span> RP.30.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 text-center">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/makanan4.jpg" alt=""></a>
+						</div>
+						<h3>blueberry cake</h3>
+						<p class="product-price"><span>Per pcs</span> RP.25.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
+					</div>
+				</div>
+				<div class="col-lg-4 col-md-6 text-center strawberry">
+					<div class="single-product-item">
+						<div class="product-image">
+							<a href="single-product.html"><img src="assets/img/products/makanan 3.jpg" alt=""></a>
+						</div>
+						<h3>kimchi fried rice</h3>
+						<p class="product-price"><span>Per porsi</span> RP.55.000 </p>
+						<a href="cart.html" class="cart-btn"><i class="fas fa-shopping-cart"></i> Add to Cart</a>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- end products -->
-	
+	</div>
+	<!-- end products -->
+
 @endsection
