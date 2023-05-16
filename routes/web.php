7664f8871fc7594b9b4229b16b3b9d5841541823
Fajar Fathurrohman <fajarfathurrohman965@gmail.com>
2023-05-16 11:24:45 +0700

    membuat pdf

--- routes/web.php
+++ routes/web.php
@@ -39,6 +39,8 @@ Route::post('/tambah-meja/sukses', [MejaController::class, 'store']);
 Route::post('/bayar/{no_pes}', [BayarController::class, 'index']);
 // admin 
 Route::get('/data-pesanan', [PesananController::class, 'index']);
+Route::get('/print-pesanan', [PesananController::class, 'pdf']);
+Route::get('/laporan', [PesananController::class, 'laporan']);
 
 Auth::routes();
 
