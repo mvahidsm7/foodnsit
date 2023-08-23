54127c015a77c84066fefd1aaa8d3644a2157402
xvhxid <mvahidsm7@gmail.com>
2023-08-23 13:39:18 +0700

    change : seeder menu & gambar

--- database/seeders/MenuSeeder.php
+++ database/seeders/MenuSeeder.php
@@ -13,48 +13,35 @@ class MenuSeeder extends Seeder
      */
     public function run(): void
     {
-        Menu::create([
-            "nama" => "Lobster Baked",
-            "gambar" => "assets\img\products\makanan1.png",
-            "deskripsi" => "Lobster yang dibakar dengan saus keju",
-            "kategori" => 3,
-            "harga" => 50000
-        ],
-        Menu::create([
-            "nama" => "Gelato",
-            "gambar" => "assets\img\products\dessert1.jpg",
-            "deskripsi" => "Gelato yang manis dan segar",
-            "kategori" => 2,
-            "harga" => 25000
-        ]),
-        Menu::create([
-            "nama" => "Iced Cinnamon Rolls",
-            "gambar" => "assets\img\products\minuman1.jpg",
-            "deskripsi" => "Iced, Cinnamon, Butter, Milk, and Vanilla",
-            "kategori" => 1,
-            "harga" => 15000
-        ]),
-        Menu::create([
-            "nama" => "Iced Coffee Latte",
-            "gambar" => "assets\img\products\minuman2.jpg",
-            "deskripsi" => "Racikan espresso dari kopi terbaik robusta Lampung",
-            "kategori" => 1,
-            "harga" => 15000
-        ]),
-        Menu::create([
-            "nama" => "Kimchi Fried Rice",
-            "gambar" => "assets\img\products\makanan2.jpg",
-            "deskripsi" => "Hidangan populer di Korea Utara",
-            "kategori" => 3,
-            "harga" => 30000
-        ]),
-        Menu::create([
-            "nama" => "Blueberry Cake",
-            "gambar" => "assets\img\products\dessert2.jpg",
-            "deskripsi" => "Kek lembut dengan selai Blueberry",
-            "kategori" => 2,
-            "harga" => 20000
-        ])
-    );
+        Menu::create(
+            [
+                "nama" => "Paket Makan Bareng 1",
+                "gambar" => "assets\img\products\paket4.jpg",
+                "deskripsi" => "sashimi,sushi roll,aburi salmon,namon belly",
+                "kategori" => 2,
+                "harga" => "100000"
+            ],
+            Menu::create([
+                "nama" => "Paket Makan Bareng 2",
+                "gambar" => "assets\img\products\paket5.jpg",
+                "deskripsi" => "Ayam Semur,beef slice asam manis,sambal jeruk,oseng toge,Salad",
+                "kategori" => 1,
+                "harga" => 25000
+            ]),
+            Menu::create([
+                "nama" => "Paket Makan Bareng 3",
+                "gambar" => "assets\img\products\paket3.jpg",
+                "deskripsi" => "Nasi Goreng katsu+telor,Chicken Katsu ,Nasi Beef Asam Manis,Salad,Saus BBQ,Pudding Matcha",
+                "kategori" => 1,
+                "harga" => 15000
+            ]),
+            Menu::create([
+                "nama" => "Paket Ngemil",
+                "gambar" => "assets\img\products\paket1.jpg",
+                "deskripsi" => "Macam Macam donat: choco cookies,greentea,cheeses,choclate cheese",
+                "kategori" => 3,
+                "harga" => 30000
+            ]),
+        );
     }
 }
