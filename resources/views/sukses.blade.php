f826c2128071d60e2c9cea240c23aff690676f21
xvhxid <mvahidsm7@gmail.com>
2023-08-24 08:52:01 +0700

    fix : konfirmasi naikin height

--- resources/views/sukses.blade.php
+++ resources/views/sukses.blade.php
@@ -1,17 +1,17 @@
 @extends('layouts.main')
 @section('content')
     <!-- breadcrumb-section -->
-    <div class="breadcrumb-section breadcrumb-bg">
+    <div class="breadcrumb-section breadcrumb-bg" style="height: 70%">
         <div class="container">
             <div class="row">
-                <div class="col-lg-8 offset-lg-2 text-center">
+                <div class="col-lg-8 offset-lg-2 text-center mt-150">
                     <div class="breadcrumb-text">
                         <hr>
                         <h1>Pesanan Berhasil di Buat</h1>
                         <hr>
                         <center>
                             <a href="/" class="btn btn-primary">Beranda</a>
-                            <a href="/profil" class="btn btn-warning">Lihat Pesanan</a>
+                            <a href="/profil" class="btn btn-info">Lihat Pesanan</a>
                             <hr>
                         </center>
                     </div>
