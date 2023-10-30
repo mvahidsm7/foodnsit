22a877a18e22470d4660d88db3bea18845d26723
Fajar Fathurrohman <137872625+Kakarott96@users.noreply.github.com>
2023-10-30 21:55:13 +0700

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -3,7 +3,7 @@
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
 @if (trim($slot) === 'Food n Sit')
-<img src="{{ asset('assets/img/logo.png') }}" class="logo" alt="FoodnSit">
+<img src="{{ asset('assets/img/logo.png') }}" class="logo" alt="">
 @else
 {{ $slot }}
 @endif
