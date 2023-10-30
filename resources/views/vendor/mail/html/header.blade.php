ee3417815db84cfeb6ff5d26e99c67216f3bb5a0
Fajar Fathurrohman <137872625+Kakarott96@users.noreply.github.com>
2023-10-30 21:31:49 +0700

    Update header.blade.php

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,7 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
+@if (trim($slot) === 'Food n Sit')
 <img src="{{ asset('assets/img/logo.png') }}" class="logo" alt="FoodnSit">
 @else
 {{ $slot }}
